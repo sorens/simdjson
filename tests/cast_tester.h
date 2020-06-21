e98c93541f0b9f5ffc25d10367e3b51b28ae520b
John Keiser <john@johnkeiser.com>
2020-06-21 11:32:11 -0700

    Make simdjson_result<element>.is() return bool

--- tests/cast_tester.h
+++ tests/cast_tester.h
@@ -35,7 +35,6 @@ public:
 
   bool test_is(element element, bool expected);
   bool test_is(simdjson_result<element> element, bool expected);
-  bool test_is_error(simdjson_result<element> element, error_code expected_error);
 
   bool test_named_get(element element, T expected = {});
   bool test_named_get(simdjson_result<element> element, T expected = {});
@@ -44,13 +43,12 @@ public:
 
   bool test_named_is(element element, bool expected);
   bool test_named_is(simdjson_result<element> element, bool expected);
-  bool test_named_is_error(simdjson_result<element> element, error_code expected_error);
 
 private:
   simdjson_result<T> named_get(element element);
   simdjson_result<T> named_get(simdjson_result<element> element);
   bool named_is(element element);
-  simdjson_result<bool> named_is(simdjson_result<element> element);
+  bool named_is(simdjson_result<element> element);
   bool assert_equal(const T& expected, const T& actual);
 };
 
@@ -187,18 +185,7 @@ bool cast_tester<T>::test_is(element element, bool expected) {
 
 template<typename T>
 bool cast_tester<T>::test_is(simdjson_result<element> element, bool expected) {
-  bool actual;
-  auto error = element.is<T>().get(actual);
-  ASSERT_SUCCESS(error);
-  ASSERT_EQUAL(actual, expected);
-  return true;
-}
-
-template<typename T>
-bool cast_tester<T>::test_is_error(simdjson_result<element> element, error_code expected_error) {
-  UNUSED bool actual;
-  auto error = element.is<T>().get(actual);
-  ASSERT_EQUAL(error, expected_error);
+  ASSERT_EQUAL(element.is<T>(), expected);
   return true;
 }
 
@@ -210,18 +197,7 @@ bool cast_tester<T>::test_named_is(element element, bool expected) {
 
 template<typename T>
 bool cast_tester<T>::test_named_is(simdjson_result<element> element, bool expected) {
-  bool actual;
-  auto error = named_is(element).get(actual);
-  ASSERT_SUCCESS(error);
-  ASSERT_EQUAL(actual, expected);
-  return true;
-}
-
-template<typename T>
-bool cast_tester<T>::test_named_is_error(simdjson_result<element> element, error_code expected_error) {
-  bool actual;
-  auto error = named_is(element).get(actual);
-  ASSERT_EQUAL(error, expected_error);
+  ASSERT_EQUAL(named_is(element), expected);
   return true;
 }
 
@@ -252,14 +228,14 @@ template<> bool cast_tester<int64_t>::named_is(element element) { return element
 template<> bool cast_tester<double>::named_is(element element) { return element.is_double(); }
 template<> bool cast_tester<bool>::named_is(element element) { return element.is_bool(); }
 
-template<> simdjson_result<bool> cast_tester<array>::named_is(simdjson_result<element> element) { return element.is_array(); }
-template<> simdjson_result<bool> cast_tester<object>::named_is(simdjson_result<element> element) { return element.is_object(); }
-template<> simdjson_result<bool> cast_tester<const char *>::named_is(simdjson_result<element> element) { return element.is_string(); }
-template<> simdjson_result<bool> cast_tester<std::string_view>::named_is(simdjson_result<element> element) { return element.is_string(); }
-template<> simdjson_result<bool> cast_tester<uint64_t>::named_is(simdjson_result<element> element) { return element.is_uint64_t(); }
-template<> simdjson_result<bool> cast_tester<int64_t>::named_is(simdjson_result<element> element) { return element.is_int64_t(); }
-template<> simdjson_result<bool> cast_tester<double>::named_is(simdjson_result<element> element) { return element.is_double(); }
-template<> simdjson_result<bool> cast_tester<bool>::named_is(simdjson_result<element> element) { return element.is_bool(); }
+template<> bool cast_tester<array>::named_is(simdjson_result<element> element) { return element.is_array(); }
+template<> bool cast_tester<object>::named_is(simdjson_result<element> element) { return element.is_object(); }
+template<> bool cast_tester<const char *>::named_is(simdjson_result<element> element) { return element.is_string(); }
+template<> bool cast_tester<std::string_view>::named_is(simdjson_result<element> element) { return element.is_string(); }
+template<> bool cast_tester<uint64_t>::named_is(simdjson_result<element> element) { return element.is_uint64_t(); }
+template<> bool cast_tester<int64_t>::named_is(simdjson_result<element> element) { return element.is_int64_t(); }
+template<> bool cast_tester<double>::named_is(simdjson_result<element> element) { return element.is_double(); }
+template<> bool cast_tester<bool>::named_is(simdjson_result<element> element) { return element.is_bool(); }
 
 template<typename T> bool cast_tester<T>::assert_equal(const T& expected, const T& actual) {
   ASSERT_EQUAL(expected, actual);
